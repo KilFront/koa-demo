40f28a5cb4cd4f47476c15d1566b707204ee33cd
聂开发 <niekaifa@apowo.com>
2021-06-17 11:07:43 +0800

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const Koa = require('koa');
 const router = require('koa-router')();
 const app = new Koa();
-const crypto = require('crypto-js');
+const crypto = require('crypto');
 // const _ = router();
 
 router.get('/hello', (ctx) => {
@@ -14,8 +14,10 @@ router.get('/wechat', (ctx) => {
   const q = ctx.query;
   const checkSignature = (q) => {
     var signature = q.signature;
+    console.log('signature', signature);
+    
     var echostr = q.echostr;
-    var token = 'shuaihua';
+    var token = 'kiltoken';
     var timestamp = q.timestamp;
     var nonce = q.nonce;
 
@@ -23,8 +25,13 @@ router.get('/wechat', (ctx) => {
     array.sort();
     var str = array.toString().replace(/,/g, "");
     var sha1Code = crypto.createHash("sha1");
+    // var sha1Code = sha1(str);
+    console.log('sha1Code', sha1Code);
+    
     var code = sha1Code.update(str, 'utf-8').digest("hex");
 
+    console.log('code', code, signature);
+    
     if (code == signature) {
         return echostr;
     }
