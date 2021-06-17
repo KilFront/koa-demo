1b098484fdd4d6d0e35c467728ed8ee0be171f6a
聂开发 <niekaifa@apowo.com>
2021-06-17 10:41:39 +0800

    wechat

--- app.js
+++ app.js
@@ -9,7 +9,29 @@ router.get('/hello', (ctx) => {
 
 router.get('/wechat', (ctx) => {
   console.log('ctx', ctx.query);
-  ctx.body = "wechat";
+
+  const q = ctx.query;
+  const checkSignature = (q) => {
+    var signature = q.signature;
+    var echostr = q.echostr;
+    var token = 'shuaihua';
+    var timestamp = q.timestamp;
+    var nonce = q.nonce;
+
+    var array = new Array(token, timestamp, nonce);
+    array.sort();
+    var str = array.toString().replace(/,/g, "");
+    var sha1Code = crypto.createHash("sha1");
+    var code = sha1Code.update(str, 'utf-8').digest("hex");
+
+    if (code == signature) {
+        return echostr;
+    }
+    return 'error';
+  }
+
+
+  ctx.body = checkSignature(q);
 })
 
 app.use(router.routes());
